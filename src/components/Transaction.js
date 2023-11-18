a85b2a953b5fc7504fd4cf2a4322ab61f9df3371
KADIRCAN BOZKURT <e2448207@ceng.metu.edu.tr>
2023-11-18 14:59:04 +0300

    help me

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,14 +3,21 @@ import { useEthersSigner } from "../utils/ethersConverter";
 import { getAllBalances } from "../utils/GetBalances";
 import { Networks } from "../utils/env";
 import { ethers } from "ethers";
+import { useNetwork, useSwitchNetwork } from "wagmi";
+import { switchNetwork } from "@wagmi/core";
+import { depositToZkSync } from "../utils/ZkSync";
+import { depositToArbitrum } from "../utils/Arbitrum";
 
-const Transaction = ({ receiver_address, amount, token, chain }) => {
+const Transaction = ({ receiver_address, amount, token, destination_chain }) => {
     const signer = useEthersSigner();
 
     const [balances, setBalances] = useState({});
     const [balancesReady, setBalancesReady] = useState(false);
 
     const [selectedChain, setSelectedChain] = useState(null);
+
+    const { switchNetwork } = useSwitchNetwork();
+    const { chain } = useNetwork();
 
     useEffect(() => {
         const fetchBalances = async () => {
@@ -24,6 +31,38 @@ const Transaction = ({ receiver_address, amount, token, chain }) => {
             fetchBalances();
         }
     }, [signer]);
+
+    const sendTransaction = async () => {
+        if (selectedChain !== chain) {
+            try {
+                const network = await switchNetwork({
+                    chainId: selectedChain,
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        if (token === "ETH") {
+            if (selectedChain === destination_chain) {
+                // internal transaction
+            } else if (selectedChain === 5) {
+                if (destination_chain === 280) {
+                    await depositToZkSync(signer, receiver_address, amount);
+                } else {
+                    await depositToArbitrum(signer, amount);
+                }
+            } else {
+                if (destination_chain === 280) {
+                    // withdraw from zkSync
+                } else {
+                    // withdraw from Arbitrum
+                }
+            }
+        } else {
+            // send ERC20
+        }
+    };
 
     return (
         <div className='page flex-row'>
@@ -52,7 +91,9 @@ const Transaction = ({ receiver_address, amount, token, chain }) => {
                     <div>Loading...</div>
                 )}
             </div>
-            <div className='d-flex'>{selectedChain !== null ? <button>Send</button> : null}</div>
+            <div className='d-flex'>
+                {selectedChain !== null ? <button onClick={sendTransaction}>Send</button> : null}
+            </div>
         </div>
     );
 };
--- src/utils/Arbitrum.js
+++ src/utils/Arbitrum.js
@@ -1,3 +1,76 @@
-export const depositToArbitrum = async (signer, receiver_address, amount, token_address) => {};
+import { EthBridger, Erc20Bridger, getL2Network, L1ToL2MessageStatus } from "@arbitrum/sdk";
+import { providers } from "ethers";
 
-export const withdrawFromArbitrum = async (signer, receiver_address, amount, token_address) => {};
+export const depositToArbitrum = async (signer, amount, token_address) => {
+    try {
+        const l2Provider = new providers.JsonRpcProvider("https://goerli-rollup.arbitrum.io/rpc");
+        const l2Network = await getL2Network(l2Provider);
+
+        if (token_address) {
+            const erc20Bridger = new Erc20Bridger(l2Network);
+            const approveTx = await erc20Bridger.approveToken({
+                l1Signer: signer,
+                erc20L1Address: token_address,
+            });
+            const approveRec = await approveTx.wait();
+            const depositTx = await erc20Bridger.deposit({
+                amount: amount,
+                erc20L1Address: token_address,
+                l1Signer: signer,
+                l2Provider: l2Provider,
+            });
+            const depositRec = await depositTx.wait();
+            const l2Result = await depositRec.waitForL2(l2Provider);
+            l2Result.complete
+                ? console.log(
+                      `L2 message successful: status: ${L1ToL2MessageStatus[l2Result.status]}`
+                  )
+                : console.log(`L2 message failed: status ${L1ToL2MessageStatus[l2Result.status]}`);
+        } else {
+            const ETHBridger = new EthBridger(l2Network);
+
+            const depositTx = await ETHBridger.deposit({
+                amount: amount,
+                l1Signer: signer,
+                l2Provider: l2Provider,
+            });
+            const depositRec = await depositTx.wait();
+            console.warn("deposit L1 receipt is:", depositRec.transactionHash);
+            console.warn("Now we wait for L2 side of the transaction to be executed ⏳");
+            const l2Result = await depositRec.waitForL2(l2Provider);
+            console.warn("L2 result is:", await l2Result.message.status());
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const withdrawFromArbitrum = async (signer, receiver_address, amount, token_address) => {
+    try {
+        const l2Provider = new providers.JsonRpcProvider("https://goerli-rollup.arbitrum.io/rpc");
+        const l2Network = await getL2Network(l2Provider);
+        if (token_address) {
+            const erc20Bridger = new Erc20Bridger(l2Network);
+            const withdrawTx = await erc20Bridger.withdraw({
+                amount: amount,
+                destinationAddress: receiver_address,
+                erc20l1Address: token_address,
+                l2Signer: signer,
+            });
+            const withdrawRec = await withdrawTx.wait();
+        } else {
+            const ETHBridger = new EthBridger(l2Network);
+
+            const withdrawTx = await ETHBridger.withdraw({
+                amount: amount,
+                l2Signer: signer,
+                destinationAddress: receiver_address,
+            });
+
+            const withdrawRec = await withdrawTx.wait();
+            console.warn("withdraw L1 receipt is:", withdrawRec.transactionHash);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};
